d10183ee3a9afc2adcb3ef1ee39a42937535561e
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-08-29 15:19:21 +0800

    inference model working

--- src/g-1-dynamic-spatial-occupancy_functions.R
+++ src/g-1-dynamic-spatial-occupancy_functions.R
@@ -16,14 +16,13 @@ mean.occ <- function(occ, k, dm){
 }
 
 # change occupancy probability according to parameters
-update.occ.prob <- function(occ.prob, dm, ext.vars, col.vars, pars){
-  #browser()
+update.occ.prob <- function(occ.prob, dm, ext.vars, col.vars, pars, sims){
   col.vars <- cbind(col.vars, mean.occ(occ.prob, pars$k, dm)) # get weighted occupancy
   if (nrow(pars$col.b) != ncol(col.vars)) stop("Dimension mismatch between variables and parameters")
   if (nrow(pars$ext.b) != ncol(ext.vars)) stop("Dimension mismatch between variables and parameters")
   pred.col <- col.vars %*% pars$col.b # predictor of log-odds of colonisation
   pred.ext <- ext.vars %*% pars$ext.b # predictor of log-odds of extinction
-  if ("greta" %in% class(occ.prob)) {
+  if (!sims) {
     (1-occ.prob)*ilogit(pred.col) + occ.prob*(1-ilogit(pred.ext)) # marginalised occupancy probability
   } else {
     (1-occ.prob)*plogis(pred.col) + occ.prob*(1-plogis(pred.ext))
@@ -32,11 +31,15 @@ update.occ.prob <- function(occ.prob, dm, ext.vars, col.vars, pars){
 
 # run over t time primary steps
 # occ.prob is JJ * (TT+1) matrix with first column giving initial occupancy at each site.
-dyn.occ <- function(init.site, nsteps, dm, ext.vars, col.vars, pars){
-  occ.prob <- matrix(0, nrow = nrow(col.vars), ncol = nsteps+1) # make occupancy matrix
+dyn.occ <- function(init.site, nsteps, dm, ext.vars, col.vars, pars, sims = TRUE){
+  if (sims){
+    occ.prob <- matrix(0, nrow = nrow(col.vars), ncol = nsteps+1) # make occupancy matrix
+  } else {
+    occ.prob <- zeros(nrow(col.vars), nsteps+1) # greta version of same  
+  }
   occ.prob[init.site, 1] <- 1 # initialise
   for (tt in 1:nsteps){
-    occ.prob[,tt+1] <- update.occ.prob(occ.prob = occ.prob[,tt], dm, ext.vars, col.vars, pars)
+    occ.prob[,tt+1] <- update.occ.prob(occ.prob = occ.prob[,tt], dm, ext.vars, col.vars, pars, sims)
   }
   occ.prob[, -1] # remove initial column to return JJ * TT matrix
 }
--- src/g-dynamic-spatial-occupancy.R
+++ src/g-dynamic-spatial-occupancy.R
@@ -4,54 +4,93 @@
 # use data structures of SpOccupancy, which are considerably nicer than those of unmarked.
 #library(greta.dynamics)
 
-###### Priors ######
-col.b <- normal(mean = 0, sd = 2, dim = c(2,1)) # colonisation coefficients (intercept and immigration)
-ext.b <- normal(mean = 0, sd = 2, dim = c(2,1)) # extinction coefficients (intercept and control effort)
+# simulate some data
+source("src/g-2-simulate-dynamic-occupancy.R")
 
-k <- uniform(0.5, 1) # rate of spatial decay
+###### Priors ######
+col.int <- normal(mean = -5, sd = 2)
+col.b <- normal(mean = 0, sd = 2) # colonisation coefficients (intercept and immigration)
+ext.int <- normal(mean = -5, sd = 2)
+ext.b <- normal(mean = 0, sd = 2) # extinction coefficients (intercept and control effort)
+det.int <- normal(mean = 0, sd = 2)
+det.b1 <- normal(mean = 0, sd = 2)
+det.b2 <- normal(mean = 0, sd = 2)
+k <- uniform(0.1, 1) # rate of spatial decay
 
 # parameter list
-p.list  <- list(col.b = col.b,
-                ext.b = ext.b,
+p.list  <- list(col.b = c(col.int, col.b),
+                ext.b = c(ext.int, ext.b),
+                det.b = c(det.int, det.b1, det.b2),
                 k = k)
 
 
 ##### Data #####
 # Set up a 1D distance matrix
-nsites <- 10
-dm <- as.matrix(dist(cbind(rep(0, nsites), 1:nsites), diag = TRUE, upper = TRUE))
-dm <- as_data(dm)
+dist.mat <- as_data(dist.mat)
 
 # initial cell
 init.cell <- 1 #initially colonised cell
-init.state <- rep(0, nsites)
+init.state <- rep(0, JJ)
 init.state[init.cell] <- 1
 init.state <- as_data(init.state)
 
-# variables for extinction and colonisation
-ev <- data.frame(intercept = rep(1, nsites),
-                 effort = rep(0, nsites)) |>
-  as_data()
-cv <- data.frame(intercept = rep(1, nsites)) |>
-  as_data()
+# variables for extinction colonisation, and detection
+ext.vars <- as_data(ext.vars)
+col.vars <- as_data(col.vars)
+det.vars <- as_data(det.vars)
 
-# n time steps
-nt <- 3
-# some made-up data
-occ.dat <- matrix(sample(0:1, size = nt*nsites, replace = TRUE), ncol = nt) |> as_data()
+# observed presence/absence at time within primary period
+n.obs.jj.tt <- apply(obs.array, MARGIN = c(1, 2), FUN = function(x){sum(!is.na(x))})
+n.obs.total <- sum(n.obs.jj.tt)
+obs.array[is.na(obs.array)] <- 9 # dummy number to replace NAs
+obs.array <- as_data(obs.array)
 
 ##### Dynamic model of occupancy over time #####
-occ.state <- dyn.occ(init.prob = c(1, rep(0, nsites-1)), nsteps = nt, dm = dm, ext.vars = ev, col.vars = cv, pars = p.list)
+occ.state <- dyn.occ(init.site = init.cell, 
+                     nsteps = TT, 
+                     dm = dist.mat, 
+                     ext.vars = ext.vars, 
+                     col.vars = col.vars, 
+                     pars = p.list, 
+                     sims = FALSE)
 
-distribution(occ.dat) <- bernoulli(occ.state[,-1]) 
+##### calculate likelihoods #####
+lhood <- zeros(n.obs.total) # to take likelihoods
+ii <- 1 # to index lhood
+for (jj in 1:JJ){
+  for (tt in 1:TT){
+    os <- occ.state[jj, tt] # Probability occupied
+    n.obs <- n.obs.jj.tt[jj, tt] # number of actual observations
+    obs.jj.tt <- obs.array[jj, tt, 1:n.obs] # vector of observations
+    dim(obs.jj.tt) <- n.obs
+    if (n.obs == 0) next
+    det.vars <- do.call("cbind", lapply(obs.vars, FUN = function(x){x[jj, tt,]}))
+    det.vars <- det.vars[1:n.obs, ,drop = FALSE]
+    det.vars <- cbind(rep(1, nrow(det.vars)), det.vars)
+    pred.det <- det.vars %*% p.list$det.b # predictor of log-odds of detection
+    prob.det <- ilogit(pred.det)
+    ind.var <- !(1 %in% obs.jj.tt)
+    lhood[ii:(ii+n.obs-1)] <- os*(prob.det*obs.jj.tt + (1-prob.det)*(1-obs.jj.tt)) + (1-os)*ind.var # insert likelihood formula
+    ii <- ii+n.obs
+  }
+}
+
+neg.llhood <- -log(lhood)
+
+zt <- rep(0, length(neg.llhood))
+
+distribution(zt) <- poisson(neg.llhood) 
 
 ##### Some plots and diagnostics #####
-o.mod <- model(occ.state)
+o.mod <- model(occ.state, col.int, col.b, ext.int, ext.b, det.int, det.b1, det.b2, k)
 
 plot(o.mod)
 
-sims <- calculate(occ.state, nsim = 5)
-sims$occ.state[1,,]
+draws <- mcmc(o.mod)
+
+
+# sims <- calculate(occ.state, nsim = 5)
+# sims$occ.state[1,,]
 
 ## Currently we have a working model in which occupancy state is observed directly.  Next layer is to add detection within primary session
 
