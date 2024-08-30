1bd37b6e66d3ca6a8639ad96fa703b39131fca24
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-08-30 08:46:22 +0800

    setup new data filters

--- src/b-data-organisation.R
+++ src/b-data-organisation.R
@@ -19,39 +19,41 @@ df <- read_xlsx(path = file.path(data_dir, "RDHBSurveillance_2024-08-12.xlsx"),
 
 ### to do, cast to sf ###
 
-# remove empty geometries and define date/time, presence/absence, select only the useful columns
+# extract the colony data
+cny.df <- df %>%
+  filter(!is.na(ColonyNumber) & ColonyNumber != 0 ) %>%
+  select(date = dateOfActivity,
+         lat = Lat,
+         long = Long,
+         contains("Comb"), 
+         contains("Cell"), 
+         contains("Colony"), 
+         DiagnosticNotes) %>%
+  st_as_sf(coords = c("long", "lat"), crs = 4326)
+
+
+# tidy up, select only the useful columns, cast to sf
 df <- df %>%
-  rename(geometry = Spatial) %>%
-  filter(!st_is_empty(geometry)) %>%
-  mutate(date.time = dmy_hm(dateOfActivity, tz = "Australia/Perth"), # date/time/hour
-         hour = hour(date.time)) %>%
-  mutate(pres = ifelse(grepl("Red", SpeciesObservedInFieldTXT), 1, 0)) %>% # present/absent data
-  select(ID, Title, date.time, hour, pres, Notes, HostLookup, SpeciesCount, # only take columns we need
-           HostOther, HostFlowering, CaseLink, SmallGridID, SurveillanceActivity, 
-           ActivityTXT, ColonyNumber, CaseTXT,
-           SpeciesActivity, 
-           SmallGridID.ID, 
-           DistanceFromRoad_meters, SlopeOrientation, 
-           DistanceFromKnownForage_meters, AltitudeInMeters, 
-           geometry)
-  
-# identify first point in the remaining geometries
-first_points <- st_sfc(lapply(df$geometry, extract_first_point))
-
-# replace the complex multipoint geometry collections with a single point
-df$geometry <- first_points
-st_crs(df) <- 4326
-rm(first_points)
-
-# trim down to only data where we have a point record
-df <- subset(df,!st_is_empty(df$geometry))
-
-# filter out spatial / temporal outliers (data entry or other issues?)
-df <- df %>%
+  filter(is.na(ColonyNumber) | ColonyNumber == 0 ) %>% # remove colony data
+  mutate(presence = ifelse(grepl("Red", SpeciesObservedValue), 1, 0),
+         hive.removed = ifelse(grepl("Colony found", SurveillanceActivityValue, ignore.case = TRUE), 1, 0)) %>% # present/absent data
+  select(date.time = dateOfActivity, 
+         lat = Lat, 
+         long = Long, 
+         dist.forage.m = DistanceFromKnownForage_meters,
+         dist.water.m = DistanceFromKnownWaterSource_met,
+         dist.road.m = DistanceFromRoad_meters,
+         flight.line.deg = FlightLineDegrees,
+         host.flower = HostFloweringValue,
+         slope.orientation = SlopeOrientation,
+         temperatue = Temperature,
+         hive.removed,
+         presence, 
+         abundance = AbundanceValue) %>%
+  filter(!is.na(lat)) %>%
+  filter(lat > -20.7) %>% # remove surprising points a long way south
   filter(year(date.time) != 2005) %>% #remove surprising year
-  mutate(lat = unlist(lapply(geometry,function(x){x[2]})), # extract lat and long for ease of access
-         long = unlist(lapply(geometry,function(x){x[1]}))) %>%
-  filter(lat > -20.7) # remove surprising points a long way south
+  st_as_sf(coords = c("long", "lat"), crs = 4326) # cast to a spatial object
 
 # calculate location of earliest record and distance from there to all other records
 # we want distance in metres, so first cast to Australian Albers (CRS = 3577)
