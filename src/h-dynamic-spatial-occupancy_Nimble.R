f68a0edced7d013b2a6aee694e95c171d949a155
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-09-11 12:30:51 +0800

    complete Nimble version of the model

--- src/h-dynamic-spatial-occupancy_Nimble.R
+++ src/h-dynamic-spatial-occupancy_Nimble.R
@@ -1,11 +1,71 @@
-# model to run dynamic spatial occupancy model on RDHB data
+# model to run dynamic spatial occupancy model on RDHB data - Using Nimble
 
-library(rjags)
+library(nimble)
 
 # get the data and organise it
 source("src/h-1-dynamic-spatial-occupancy_data-organisation.R")
 
-# arrange the data for JAGS
+# organise JAGS code into Nimble Code
+dso.code <- nimbleCode(
+  {
+    # priors
+    rho.int ~ dnorm(0, 1/4) # initial occupancy
+    rho.b ~ dnorm(0, 1/4)
+    col.int ~ dnorm(-5, 1/4)
+    col.b ~ dnorm(0, 1/36) # colonisation coefficients (intercept and immigration)
+    ext.int ~ dnorm(-5, 1/9)
+    ext.b ~ dnorm(0, 1/4) # extinction coefficients (intercept and control effort)
+    det.int ~ dnorm(0, 1/4)
+    det.b1 ~ dnorm(0, 1/4)
+    det.b2 ~ dnorm(0, 1/4)
+    det.b3 ~ dnorm(0, 1/4)
+    det.b4 ~ dnorm(0, 1/4)
+    k ~ dunif(0.1, 1) # rate of spatial decay
+    
+    ### process model
+    ## initial occupied cells (immediately prior to observations)
+    for (jj in 1:JJ){
+      logit(rho[jj]) <- rho.int + rho.b*init.dist[jj]
+      occ[jj, 1] ~ dbern(rho[jj])
+    }
+    
+    # spatial decay
+    for (jj in 1:JJ){
+      for (ii in 1:JJ){
+        decay.jj.ii[jj, ii] <- exp(-k*dist.mat[jj,ii])
+      }
+    }
+    
+    ## state variable over time
+    # dynamics prior to and after observation
+    for (tt in 2:(TT+1)){
+      for (jj in 1:JJ){
+        sp.jj.tt[jj, tt] <- sum(decay.jj.ii[jj, 1:JJ]*occ[1:JJ, tt-1]) # spatial colonisation effect
+        logit(p.col[jj, tt]) <- col.int + col.b*sp.jj.tt[jj, tt]
+        logit(p.ext[jj, tt]) <- ext.int + ext.b*ext.x1[jj, tt-1]
+        ext.col[jj, tt] <- (1-occ[jj,tt-1])*p.col[jj, tt] + 
+          occ[jj, tt-1]*(1-p.ext[jj, tt])
+        occ[jj,tt] ~ dbern(ext.col[jj, tt])
+      }
+    }
+    
+    # observation model
+    for (tt in 1:TT){
+      for (jj in 1:JJ){
+        for (kk in 1:KK){
+          logit(p.obs[jj, tt, kk]) <- det.int + 
+            det.b1*det.x1[jj, tt, kk] + 
+            det.b2*det.x2[jj, tt, kk] +
+            det.b3*det.x3[jj, tt, kk] +
+            det.b4*det.x4[jj, tt, kk]
+          obs[jj, tt, kk] ~ dbern(p.obs[jj, tt, kk]*occ[jj, tt+1])
+        }
+      }
+    }
+  }
+)
+
+# arrange the data for Nimble
 # given a list, output named objects for JAGS
 assign_list <- function(ls, prefix){
   for (ll in 1:length(ls)){
@@ -29,6 +89,7 @@ data.list <- list(init.dist = init.dist,
                   obs = y,
                   JJ = JJ,
                   TT = TT,
+                  KK = KK,
                   dist.mat = dist.mat)
 
 # initials
@@ -37,32 +98,37 @@ occ.init <- occ.init+0
 
 init.list <- list(occ = cbind(rep(1, JJ),occ.init))
 
+
+# parameters to monitor
+params <- c("rho.int",
+                   "rho.b",
+                   "det.int", 
+                   "det.b1", 
+                   "det.b2",
+                   "det.b3",
+                   "det.b4",
+                   "col.int",
+                   "col.b",
+                   "ext.int",
+                   "ext.b",
+                   "k")
+
+# mcmc settings
+nb <- 10
+ni <- 10
+nc = 3
+
 # the model
-a <- jags.model(file = "src/model-files/invasion-occ_RDHB_JAGS.txt", 
-                data = data.list, 
-                inits = init.list,
-                n.chains = 3)
+a <- nimbleMCMC(code = dso.code, 
+                init = init.list, 
+                monitors = params, 
+                constants = data.list, 
+                niter = ni, 
+                nburnin = nb, 
+                nchains = nc)
 
-update(a, n.iter = 5000) # burn in
-b<-coda.samples(a, 
-                variable.names = c("rho.int",
-                                   "rho.b",
-                                   "det.int", 
-                                   "det.b1", 
-                                   "det.b2",
-                                   "det.b3",
-                                   "det.b4",
-                                   "col.int",
-                                   "col.b",
-                                   "ext.int",
-                                   "ext.b",
-                                   "k"), 
-                n.iter = 10000, 
-                thin = 5)
-
-gelman.diag(b)
+coda:gelman.diag(a)
+summary(a)
 
 
-summary(b)
 
-save(a, b, file = "out/dynamic-spatial-occupancy_RCHB_Coda.RData")
