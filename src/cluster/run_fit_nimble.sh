ef8c713e1707849aba44efa84314df4334c16244
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-10-17 16:13:21 +0800

    add shell file to run nimble

---
+++ src/cluster/run_fit_nimble.sh
@@ -0,0 +1,14 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1103
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBatch --mem=200G
+ 
+# Project directory
+cd $MYSCRATCH/RDHB/RDHB_Occupancy
+
+# Run setup
+. src/cluster/a-setup-environment.sh
+Rscript src/h-dynamic-spatial-occupancy_Nimble.R
