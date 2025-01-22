b1cf6f98347a45570e5a42d0cb6ad534076d7f5d
Ben Phillips <ben.l.phillips@curtin.edu.au>
2025-01-23 07:00:43 +0800

    simulate dynamics over multiple time intervals

--- src/i-1-simulate-pp.R
+++ src/i-1-simulate-pp.R
@@ -9,6 +9,7 @@ pairwise_distances <- function(A, B) {
 
 # function to tell us if a point is inside or outside a polygon.
 # Uses the ray casting algorithm, heading right from the test point.
+# this function is too much; have switched to raster approach (see raster_to_points)
 inside <- function (pt, pg){
   edge.in <- function(pt, pg){ # Is this an edge that we will pass through?
     pg2 <- rbind(pg[-1,], pg[1,])
@@ -35,8 +36,8 @@ raster_to_points <- function(pts, hab.rast, rast.scale){
 }
 
 # function to calculate probability of at least one being present from distances (from AHM 10.8)
-spatial_decay <- function(distance, sigma, lambda.0){
-  lambda.ij <- lambda.0*exp(-distance^2/(2*sigma^2)) # poisson encounter rate with distance
+spatial_presence <- function(distance, sigma.u, u.0){
+  lambda.ij <- u.0*exp(-distance^2/(2*sigma.u^2)) # poisson encounter rate with distance
   lambda.i <- apply(lambda.ij, 1, sum) # sum over colonies
   prob.pres.i <- 1-exp(-lambda.i) # collapsed to a binary
   prob.pres.i
@@ -50,8 +51,11 @@ beta.det <- 2 # slope of some linear effect on detection
 #alpha.extent <- 50 # intercept for change in extent per time period
 #beta.extent <- -10 # effect of control on extent growth per time period
 #sd.extent <- 30 # standard deviation of random effect on change in extent per time period
-sigma <- 500 # parameter affecting spread of the density kernel
-lambda.0 <- 300 # expected number of encounters at 0 distance from a colony
+sigma.u <- 500 # parameter affecting spread of the density kernel
+u.0 <- 300 # expected number of encounters at 0 distance from a colony
+alpha.lambda <- log(1.5) # intercept of growth rate function
+beta.lambda <- 0.1 # slope of the growth rate function
+sigma.d <- 900
 
 # some setup variables
 nt <- 1 # number of time steps
@@ -73,29 +77,62 @@ g0.y <- 0.5*y.max
 # initialisation for dynamic state variables
 r0 <- 0.2*max(c(x.max, y.max)) # radius of invasion extent at time = 0, in m
 c.n <- density*x.max*y.max # number of "colonies"
-c.j0.x <- runif(n = c.n, min = x.min, max = x.max) # place initial colonies at time 0
-c.j0.y <- runif(n = c.n, min = y.min, max = y.max)
-c.0 <- cbind(c.j0.x, c.j0.y) # matrix of colony locations
+c.0.x <- runif(n = c.n, min = x.min, max = x.max) # place initial colonies at time 0
+c.0.y <- runif(n = c.n, min = y.min, max = y.max)
+c.0 <- cbind(c.0.x, c.0.y) # matrix of colony locations
 z0 <- pairwise_distances(c.0, cbind(g0.x, g0.y)) < r0 # distances from g0 < r0 (i.e. which colonies are real.)
 z0 <- z0 * raster_to_points(c.0, mask.raster, rast.scale = x.max/2)
 c.0 <- c.0[z0==1,]
 s.n <- survey.density*x.max*y.max # number of surveys
-s.i0.x <- runif(n = s.n, min = x.min, max = x.max) # place initial surveys at time 0
-s.i0.y <- runif(n = s.n, min = y.min, max = y.max)
-s.0 <- cbind(s.i0.x, s.i0.y) # matrix of survey locations
-s.0 <- s.0[raster_to_points(s.0, mask.raster, rast.scale = x.max/2)==1, ]
+
+generate_surveys <- function(){
+  s.i0.x <- runif(n = s.n, min = x.min, max = x.max) # place surveys
+  s.i0.y <- runif(n = s.n, min = y.min, max = y.max)
+  s.0 <- cbind(s.i0.x, s.i0.y) # matrix of survey locations
+  s.0 <- s.0[raster_to_points(s.0, mask.raster, rast.scale = x.max/2)==1, ] # remove those outside mask
+  s.0
+}
 
 # generate data
-t.step <- rep(1:nt, each = nrow(s.0)) # vector of time steps
-sur.lev.var <- rnorm(length(t.step)) # survey level variable (affects detection)
-logit.det <- alpha.det + beta.det*sur.lev.var # linear model on detection
-det <- plogis(logit.det) # conditional detection probability
+## time step 1
+# function that uses global variables to generate data from current s, and c.
+generate_data <- function(s.t, c.t, t = 1) {
+  sur.lev.var <- rnorm(nrow(s.t)) # survey level variable (affects detection)
+  logit.det <- alpha.det + beta.det*sur.lev.var # linear model on detection
+  det <- plogis(logit.det) # conditional detection probability
+    
+  d.ij <- pairwise_distances(s.t, c.t) # distances between surveys and colonies
   
-d.ij <- pairwise_distances(s.0, c.0) # distances between surveys and colonies
+  p.pres.i <- spatial_presence(d.ij, sigma.u, u.0)
+  obs.i <- rbinom(n = length(p.pres.i), size = 1, prob = p.pres.i*det) # generate observations
+  cbind(time = rep(t, length(p.pres.i)), x = s.t[,1], y = s.t[,2], det.var = sur.lev.var, obs = obs.i) # output
+}
 
-p.pres.i <- spatial_decay(d.ij, sigma, lambda.0)
-obs.i <- rbinom(n = length(p.pres.i), size = 1, prob = p.pres.i*det)
+sim.dat <- generate_data(generate_surveys(), c.0)
 
+# Grows the population according to lambda.t and sigma.d
+pp_growth <- function(c.t, lambda.t, sigma.d){
+  n.0 <- nrow(c.t)
+  n.1 <- rpois(n.0, lambda.t[tt-1]) # realised number of daughter colonies
+  inds <- rep(1:n.0, times = n.1)
+  c.1 <- c.t[inds,] # new matrix of colonies, pre dispersal
+  n.1 <- nrow(c.1)
+  c.1[,1] <- rnorm(n = n.1, mean = c.1[,1], sd = sigma.d) # disperse with gaussian kernel
+  c.1[,2] <- rnorm(n = n.1, mean = c.1[,2], sd = sigma.d)
+  z0 <- raster_to_points(c.1, mask.raster, rast.scale = x.max/2) #
+  c.1[z0==1,] # remove those outside mask
+}
 
-sum(obs.i)
-plot(s.0[,2]~s.0[,1], col = 1+obs.i)
+## time step >2
+if (nt > 1){
+  c.t <- c.0
+  lambda.x <- rnorm(nt) # some covariate of lambda
+  lambda.t <- exp(alpha.lambda + beta.lambda*lambda.x) #lambda as a function of time step
+  for (tt in 2:nt){
+     c.t <- pp_growth(c.t, lambda.t[tt], sigma.d = sigma.d)
+     s.t <- generate_surveys()
+     sim.dat <- rbind(sim.dat, generate_data(s.t, c.t, t = tt))
+  }  
+}
+
+##### Up to here #####
